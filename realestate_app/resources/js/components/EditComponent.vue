7843678537c3672bedfbb7a974b0ad4c935695f0
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2021-07-07 17:42:37 -0700

    added edit modal

---
+++ realestate_app/resources/js/components/EditComponent.vue
@@ -0,0 +1,105 @@
+<template>
+    <div>
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+
+                <!--body-->
+                <form @submit.prevent="createAlbum" method="POST" action="" enctype="multipart/form-data">
+
+                    <div class="form-group row">
+                        <label for="name" class="col-md-4 col-form-label text-md-left">Album Name</label>
+                        <div class="col-md-12">
+                            <input id="name" type="text" class="form-control" name="name" autocomplete="name" v-model="editrecord.name" required autofocus>
+
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="description" class="col-md-4 col-form-label text-md-left">Description</label>
+
+                        <div class="col-md-12">
+                            <textarea id="description"  class="form-control" name="description"  v-model="editrecord.description"></textarea>
+
+                        </div>
+                    </div>
+
+
+                    <div class="form-group row">
+                        <label for="category" class="col-md-4 col-form-label text-md-left">Category</label>
+
+                        <div class="col-md-12">
+                            <select id="category"  class="form-control" name="category" v-model="editrecord.category_id"  >
+                                <option value="">Select Category</option>
+                                <option v-for="(category, index) in categories" :key="index" :value="category.id" >
+                                    {{category.name}}
+                                </option>
+                            </select>
+                        </div>
+                    </div>
+
+
+                    <div class="form-group row">
+                        <label for="image" class="col-md-4 col-form-label text-md-left"></label>
+
+                        <div class="col-md-12">
+                            <input id="image" type="file"  class="form-control" name="image" >
+                        </div>
+
+                    </div>
+
+                    <div class="form-group row">
+                        <div class="col-md-12">
+                            <button id="submit" type="submit"  class="btn btn-dark" name="submit">Save</button>
+                        </div>
+                    </div>
+                </form>
+
+
+                <!--body end-->
+            </div>
+            <!--
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+            -->
+            </div>
+        </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    props:["editrecord"],
+    data() {
+        return {
+            categories:[]
+        }
+    },
+    created() {
+        this.getCategories
+    },
+    methods: {
+        getCategories()
+        {
+            axios.get('/api/categories').then((response)=>{
+               this.categories = response.data
+            }).catch((error)=>{
+                alert('Unable to fetch data.')
+            })
+        },
+    },
+}
+</script>
